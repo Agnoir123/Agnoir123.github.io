d6a31e8a5ecfe2f7b9403ffde2e4c0483fb6ecad
Agnoir123 <131035126+Agnoir123@users.noreply.github.com>
2023-10-01 23:59:46 +0100

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,23 @@
+// api/proxy.js
+const fetch = require('node-fetch');
+
+export default async function(req, res) {
+  const url = 'https://script.google.com/macros/s/AKfycbw8K2wgXTepS4eZyjkxx-L4YgWIobURpYKcxT8cwquuuItlwszfT54crr_10pE0vbcU/exec';
+  
+  const formData = req.body;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
